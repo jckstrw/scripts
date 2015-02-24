6367a229d42718b99583e3fa9c05ff4d674eb230
Jim M <jmatthews@sovrn.com>
2015-02-24 16:26:35 -0700

    initial checkin

--- create/create_vm.sh
+++ create/create_vm.sh
@@ -14,6 +14,7 @@ echo "This script is used to create a VM."
 echo "Some of the prompts have a default value, in parenthesis." 
 echo "Accept the default value by hitting return or input your own."
 echo " "
+
 read -p "Name of VM: " NAME
 read -p "RAM (2): " RAM
 RAM=${RAM:-2}
@@ -28,14 +29,15 @@ then
 fi
 read -p "Disk Size in GB (16): " DISKSIZE
 DISKSIZE=${DISKSIZE:-16}
-read -p "Bridge: " BRIDGE
+read -p "Bridge (br10): " BRIDGE
+BRIDGE=${BRIDGE:-br10}
 read -p "Installer (trusty): " INSTALLER
 INSTALLER=${INSTALLER:-http://repo2.15c.lijit.com/install/ubuntu/trusty/installer-amd64}
 read -p "IP: " IP
 read -p "Netmask: " MASK
 read -p "Gateway: " GW
-read -p "Hostname: " HOSTNAME
+read -p "FQ Hostname: " HOSTNAME
 read -p "Domain: " DOMAIN 
-read -p"Preseed Image: " IMAGE 
+read -p "Preseed Image: " IMAGE 
 
-echo "virt-install -n $NAME -r $RAME --vcpus=$CPU --os-type=linux --disk path=$DISKPATH,bus=virtio,format=raw,sparse=false,size=$DISKSIZE -w bridge=$BRIDGE,model=virtio --accelerate --virt-type kvm --location=http:$INSTALLER --vnc --extra-args="netcfg/disable_autoconfig=true netcfg/get_nameservers=10.100.10.31 netcfg/get_ipaddress=$IP netcfg/get_netmask=$MASK netcfg/get_gateway=$GW netcfg/get_hostname=$HOSTNAME netcfg/get_domain=$DOMAIN auto=true url=$IMAGE console=ttyS0,115200n8" "
+echo "virt-install -n $NAME -r $RAM --vcpus=$CPU --os-type=linux --disk path=$DISKPATH,bus=virtio,format=raw,sparse=false,size=$DISKSIZE -w bridge=$BRIDGE,model=virtio --accelerate --virt-type kvm --location=http:$INSTALLER --vnc --extra-args="netcfg/disable_autoconfig=true netcfg/get_nameservers=10.100.10.31 netcfg/get_ipaddress=$IP netcfg/get_netmask=$MASK netcfg/get_gateway=$GW netcfg/get_hostname=$HOSTNAME netcfg/get_domain=$DOMAIN auto=true url=$IMAGE console=ttyS0,115200n8" "
