2b4e04ce088bc2d14efac319c1588932563d1187
Jim M <jmatthews@sovrn.com>
2015-02-25 11:10:03 -0700

    tweaks

--- create/create_vm.py
+++ create/create_vm.py
@@ -4,6 +4,8 @@
 #
 # jmm - 2015-02-24
 import os
+import os.path
+import sys
 #if os.geteuid() != 0:
 #    exit("You must be root to run this")
 os.system('clear')
@@ -13,22 +15,16 @@ print ('Accept the default value by hitting return or input your own.')
 print ('')
 
 name 		= raw_input('Name of VM: ')
-ram 		= raw_input('RAM (2048): ')
-cpu		= raw_input('CPU (1): ')
+ram 		= raw_input('RAM (2048): ') or '2048'
+cpu		= raw_input('CPU (1): ') or '1'
 diskpath	= raw_input('Disk Path: ')
-#DISKPATH=${DISKPATH}
-#if [ -f $DISKPATH ];
-#then
-#	echo "Don't be Matt. This file already exists."
-#	exit 1
-#fi
 
-disksize	= raw_input('Disk Size in GB (16): ')
-#DISKSIZE=${DISKSIZE:-16}
-bridge		= raw_input('Bridge (br10): ')
-#BRIDGE=${BRIDGE:-br10}
-installer	= raw_input('Installer (trusty): ')
-#INSTALLER=${INSTALLER:-http://repo2.15c.lijit.com/install/ubuntu/trusty/installer-amd64}
+if os.path.isfile(diskpath): 
+	sys.exit ('Don\'t be Matt. The file already exists')
+
+disksize	= raw_input('Disk Size in GB (16): ') or '16'
+bridge		= raw_input('Bridge (br10): ') or 'br10'
+installer	= raw_input('Installer (trusty): ') or 'http://repo2.15c.lijit.com/install/ubuntu/trusty/installer-amd64'
 ip		= raw_input('IP: ')
 mask		= raw_input('Netmask: ')
 gw		= raw_input('Gateway: ')
@@ -38,5 +34,6 @@ image		= raw_input('Preseed Image: ')
 
 print ("") 
 print ('Here is the command to execute:')
-
-print ('virt-install -n ' + name + ' -r ' + ram + ' --vcpus=cpu --os-type=linux --disk path=' + diskpath + ',bus=virtio,format=raw,sparse=false,size=' + disksize + ' -w bridge=' + bridge +',model=virtio --accelerate --virt-type kvm --location=' + installer + ' --vnc --extra-args=\"netcfg/disable_autoconfig=true netcfg/get_nameservers=10.100.10.31 netcfg/get_ipaddress=' + ip + ' netcfg/get_netmask=' + mask + ' netcfg/get_gateway=' + gw + ' netcfg/get_hostname=' + hostname + ' netcfg/get_domain=' + domain + ' auto=true url=' + image + ' console=ttyS0,115200n8\"')
+print ("")
+print ('virt-install -n ' + name + ' -r ' + ram + ' --vcpus=' + cpu + ' --os-type=linux --disk path=' + diskpath + ',bus=virtio,format=raw,sparse=false,size=' + disksize + ' -w bridge=' + bridge +',model=virtio --accelerate --virt-type kvm --location=' + installer + ' --vnc --extra-args=\"netcfg/disable_autoconfig=true netcfg/get_nameservers=10.100.10.31 netcfg/get_ipaddress=' + ip + ' netcfg/get_netmask=' + mask + ' netcfg/get_gateway=' + gw + ' netcfg/get_hostname=' + hostname + ' netcfg/get_domain=' + domain + ' auto=true url=' + image + ' console=ttyS0,115200n8\"')
+print ("")
